30292a07fa2c98386f282a8a61584835559f8899
Brian Kirk <bmarshallk@gmail.com>
2019-04-01 19:49:20 -0600

    Refactor: Add initial code

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,11 +20,34 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+import collections
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
+#Cutting down the numbers of items to train on (speed up training)
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+classifier = SVC(C=10000.0, kernel='rbf')
+t0 = time()
+classifier.fit(features_train, labels_train)
+print 'Classifier time: ', round(time()-t0,3), 's'
+
+t1 = time()
+predictions = classifier.predict(features_test)
+print 'Prediction time: ', round(time()-t1,3), 's'
+
+score = accuracy_score(labels_test, predictions, normalize=True)
+print 'The accuracy is: %s' % score
+
+#How many items are Chris's email:
+counter = collections.Counter(predictions)
+chris = counter[1]
+sarah = counter[0]
+
+print 'The number of emails by Chris is: ', chris
 #########################################################
 
 
