91c2b9f42716fe54b9a0014c3af1e13fdc13e5f0
Brian Kirk <bmarshallk@gmail.com>
2019-04-07 13:19:35 -0600

    Feat: Add initial code

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,11 +19,22 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+classifier = tree.DecisionTreeClassifier(min_samples_split=40)
+t0 = time()
+trained_classifier = classifier.fit(features_train, labels_train)
+print 'Training time: %s' % round((time() - t0),2)
+
+t1 = time()
+predictions = trained_classifier.predict(features_test)
+print 'Prediction time: %s' % round((time() - t1),2)
+
+accuracy = accuracy_score(predictions, labels_test, normalize=True)
+print accuracy
 
 
 #########################################################
