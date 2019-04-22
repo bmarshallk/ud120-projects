2149be339d8d2c32863c52e2ef77f487a3e4041d
Brian Kirk <bmarshallk@gmail.com>
2019-04-22 10:25:16 -0600

    Feat: adding a stemmer to sift words

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -23,20 +23,25 @@ def parseOutText(f):
     ### split off metadata
     content = all_text.split("X-FileName:")
     words = ""
+    stems = []
     if len(content) > 1:
         ### remove punctuation
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        #words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
+	#splitting the text string into individual words
+	text_split = text_string.split()	
 
-
-
+	#using a stemmer to reduce word-dimensionality in the incoming emails
+	stemmer = SnowballStemmer('english')
+        for word in text_split:
+		stems.append(stemmer.stem(word))
+	words = ' '.join(stems)
 
     return words
 
